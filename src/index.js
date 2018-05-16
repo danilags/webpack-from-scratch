5f44d62e1e6a43d2e128bc2055350695289031c8
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-05-16 16:19:19 +0700

    main react component:

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("hello Daniel Agus Sidabutar Webpack");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Index = () => {
+    return <div>Hello React Webpack</div>
+}
+
+ReactDOM.render(<Index />, document.getElementById('index'))
