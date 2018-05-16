c2bb9279df2bcf415c7aa4c1e540da823a1a999e
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-05-16 17:52:51 +0700

    delete dist folder

---
+++ src/component/Search/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import style from "./Search.css"
+
+const Search = () => {
+  return (
+    <div className={style.search__container}>
+      <h3>Hello Webpack :) I'am Daniel</h3>
+    </div>
+  );
+}
+
+export default Search;
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Search from './component/Search';
+
 const Index = () => {
-    return <div>Hello React Webpack</div>
+    return (
+        <Search />
+    )
 }
 
 ReactDOM.render(<Index />, document.getElementById('index'))
